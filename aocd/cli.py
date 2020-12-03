102cccd0763299f119654d3a6653f99e24ccefe3
wim glenn <hey@wimglenn.com>
2020-12-03 01:23:23 -0600

    allow year/day to be specified out of order, convenience

--- aocd/cli.py
+++ aocd/cli.py
@@ -13,13 +13,14 @@ from .utils import AOC_TZ
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Advent of Code Data")
     aoc_now = datetime.datetime.now(tz=AOC_TZ)
+    days = range(1, 26)
+    years = range(2015, aoc_now.year + int(aoc_now.month == 12))
+    parser = argparse.ArgumentParser(description="Advent of Code Data")
     parser.add_argument(
         "day",
         nargs="?",
         type=int,
-        choices=range(1, 26),
         default=min(aoc_now.day, 25) if aoc_now.month == 12 else 1,
         help="1-25 (default: %(default)s)",
     )
@@ -27,10 +28,15 @@ def main():
         "year",
         nargs="?",
         type=int,
-        choices=range(2015, aoc_now.year + int(aoc_now.month == 12)),
         default=most_recent_year(),
-        help=">= 2015 (default: %(default)s)",
+        help="2015-{} (default: %(default)s)".format(years[-1]),
     )
     args = parser.parse_args()
+    if args.day in years and args.year in days:
+        # be forgiving
+        args.day, args.year = args.year, args.day
+    if args.day not in days or args.year not in years:
+        parser.print_usage()
+        parser.exit(1)
     data = get_data(day=args.day, year=args.year)
     print(data)
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,15 +5,24 @@ from aocd.cli import main
 
 def test_main_invalid_date(mocker, capsys):
     mocker.patch("sys.argv", ["aocd", "1", "2014"])
-    with pytest.raises(SystemExit(2)):
+    with pytest.raises(SystemExit(1)):
         main()
     out, err = capsys.readouterr()
-    msg = "aocd: error: argument year: invalid choice: 2014 (choose from 2015, 2016, 2017, 2018"
-    assert msg in err
+    assert "usage: aocd [-h] [day] [year]" in out
 
 
 def test_main_valid_date(mocker, capsys):
     mocker.patch("sys.argv", ["aocd", "8", "2015"])
+    getter = mocker.patch("aocd.cli.get_data", return_value="stuff")
+    main()
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert out == "stuff\n"
+    getter.assert_called_once_with(year=2015, day=8)
+
+
+def test_main_valid_date_forgiving(mocker, capsys):
+    mocker.patch("sys.argv", ["aocd", "2015", "8"])
     getter = mocker.patch("aocd.cli.get_data", return_value="stuff")
     main()
     out, err = capsys.readouterr()
