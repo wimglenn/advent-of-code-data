6f22be84a93334695271998dedb81cc6404dbdc3
wim glenn <hey@wimglenn.com>
2020-12-05 22:58:30 -0600

    meh.. skip it

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,5 @@
+import platform
+import pytest
 from aocd.utils import blocker
 from freezegun import freeze_time
 
@@ -10,6 +12,10 @@ def test_blocker(capsys):
     assert " Unlock day 1 at " in out
 
 
+cpython = platform.python_implementation() == "CPython"
+
+
+@pytest.mark.skipif(not cpython, reason="freezegun is not working on pypy")
 @freeze_time("2020-11-30 23:59:59.8-05:00", auto_tick_seconds=1)
 def test_blocker_quiet(capsys):
     blocker(dt=0.2, quiet=True)
