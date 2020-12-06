8b7a3c64cb0447938c841aa2ec3e4fab35340b11
wim glenn <hey@wimglenn.com>
2020-12-05 23:29:14 -0600

    come on...

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,8 +16,8 @@ cpython = platform.python_implementation() == "CPython"
 
 
 @pytest.mark.skipif(not cpython, reason="freezegun is not working on pypy")
-@freeze_time("2020-11-30 23:59:59.8-05:00", auto_tick_seconds=1)
 def test_blocker_quiet(capsys):
-    blocker(dt=0.2, quiet=True)
+    with freeze_time("2020-11-30 23:59:59.8-05:00", auto_tick_seconds=1):
+        blocker(dt=0.2, quiet=True)
     out, err = capsys.readouterr()
     assert not out
