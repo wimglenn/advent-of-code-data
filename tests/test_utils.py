86b26b5fd0dab3e1f37909414978c7a6688cd380
wim glenn <hey@wimglenn.com>
2020-12-06 19:06:33 -0600

    fix pypy tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,18 +7,18 @@ from freezegun import freeze_time
 cpython = platform.python_implementation() == "CPython"
 
 
-# 0.2 second before unlock day 1
 @pytest.mark.xfail(not cpython, reason="freezegun auto-tick is not working on pypy")
-@freeze_time("2020-11-30 23:59:59.8-05:00", tick=True)
 def test_blocker(capsys):
-    blocker(dt=0.2)
+    with freeze_time("2020-11-30 23:59:59.8-05:00", tick=True):
+        # 0.2 second before unlock day 1
+        blocker(dt=0.2)
     out, err = capsys.readouterr()
     assert " Unlock day 1 at " in out
 
 
 @pytest.mark.xfail(not cpython, reason="freezegun auto-tick is not working on pypy")
-@freeze_time("2020-11-30 23:59:59.8-05:00", auto_tick_seconds=1)
 def test_blocker_quiet(capsys):
-    blocker(dt=0.2, quiet=True)
+    with freeze_time("2020-11-30 23:59:59.8-05:00", auto_tick_seconds=1):
+        blocker(dt=0.2, quiet=True)
     out, err = capsys.readouterr()
     assert not out
