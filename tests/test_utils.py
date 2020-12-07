3033bfd3af5a8001a60e787d2e7412fa3b833407
wim glenn <hey@wimglenn.com>
2020-12-06 19:31:02 -0600

    update xfail msg

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ from freezegun import freeze_time
 cpython = platform.python_implementation() == "CPython"
 
 
-@pytest.mark.xfail(not cpython, reason="freezegun auto-tick is not working on pypy")
+@pytest.mark.xfail(not cpython, reason="freezegun tick is not working on pypy")
 def test_blocker(capsys):
     with freeze_time("2020-11-30 23:59:59.8-05:00", tick=True):
         # 0.2 second before unlock day 1
@@ -16,7 +16,7 @@ def test_blocker(capsys):
     assert " Unlock day 1 at " in out
 
 
-@pytest.mark.xfail(not cpython, reason="freezegun auto-tick is not working on pypy")
+@pytest.mark.xfail(not cpython, reason="freezegun auto-tick is not working on pypy either")
 def test_blocker_quiet(capsys):
     with freeze_time("2020-11-30 23:59:59.8-05:00", auto_tick_seconds=1):
         blocker(dt=0.2, quiet=True)
