4e4c1bca9c46ec17f67467545aaf1420ac3083f9
patrick xia <patrick.xia@gmail.com>
2022-11-29 20:23:14 -0800

    remove dependency on six

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -3,8 +3,8 @@ import logging
 import pytest
 import io
 import os
+import sys
 import threading
-from six import PY2
 
 import aocd
 from aocd.exceptions import AocdError
@@ -157,6 +157,7 @@ def test_race_on_download_data(mocker, aocd_data_dir, requests_mock):
             return res
         return open_impl
     mocker.patch('io.open', side_effect=generate_open(io.open))
+    PY2 = sys.version_info.major < 3
     mocker.patch('__builtin__.open' if PY2 else 'builtins.open', side_effect=generate_open(open))
 
     t = threading.Thread(target=aocd.get_data, kwargs={'year': 2018, 'day': 1})
