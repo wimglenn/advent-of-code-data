8bd78b0882254a3cc6969b03ab640f6ab5bbe187
Cal Jacobson <cj81499@users.noreply.github.com>
2023-11-26 12:33:45 -0600

    refactor compat module

--- aocd/_compat.py
+++ aocd/_compat.py
@@ -1,14 +1,21 @@
 from importlib.metadata import entry_points
 import sys
 
+# typing.Self added in 3.11
 if sys.version_info >= (3, 11):
     from typing import Self as Self # import using same name to tell the type checker we intend to export this (so other modules can import it)
 else:
     from typing_extensions import Self as Self # import using same name to tell the type checker we intend to export this (so other modules can import it)
 
+# typing.ParamSpec added in 3.10
+if sys.version_info >= (3, 10):
+    from typing import ParamSpec as ParamSpec # import using same name to tell the type checker we intend to export this (so other modules can import it)
+else:
+    from typing_extensions import ParamSpec as ParamSpec # import using same name to tell the type checker we intend to export this (so other modules can import it)
+
+# importlib.metadata.EntryPoints added in 3.10
 if sys.version_info >= (3, 10):
     from importlib.metadata import EntryPoints
-    from typing import ParamSpec as ParamSpec # import using same name to tell the type checker we intend to export this (so other modules can import it)
 
     # Python 3.10+ - group/name selectable entry points
     def get_entry_points(group: str, name: str) -> EntryPoints:
@@ -21,8 +28,6 @@ if sys.version_info >= (3, 10):
         return entry_points(group=group)
 else:
     from importlib.metadata import EntryPoint
-
-    from typing_extensions import ParamSpec as ParamSpec # import using same name to tell the type checker we intend to export this (so other modules can import it)
 
     # Python 3.9 - dict interface
     def get_entry_points(group: str, name: str) -> list[EntryPoint]:
